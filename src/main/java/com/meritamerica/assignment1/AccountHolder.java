9c7bb8b55d84a06adae6927d4f14817b98627206
Travis Wombles <womblestravis@gmail.com>
2020-10-04 19:12:54 -0500

    Added some preliminary code into project.

--- src/main/java/com/meritamerica/assignment1/AccountHolder.java
+++ src/main/java/com/meritamerica/assignment1/AccountHolder.java
@@ -2,4 +2,67 @@ package com.meritamerica.assignment1;
 
 public class AccountHolder {
 	
+	String firstName;
+	String middleName;
+	String lastName;
+	String ssn;
+	double checkingAccountOpeningBalance;
+	double savingsAccountOpeningBalance;
+	
+	public AccountHolder(String firstName, String middleName, String lastName, String ssn, 
+						double checkingAccountOpeningBalance, double savingsAccountOpeningBalance) {
+		this.firstName = firstName;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.ssn = ssn;
+		this.checkingAccountOpeningBalance = checkingAccountOpeningBalance;
+		this.savingsAccountOpeningBalance = savingsAccountOpeningBalance;
+		
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	private void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getMiddleName() {
+		return middleName;
+	}
+	
+	private void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	private void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	private String getSSN() {
+		return ssn;
+	}
+	
+	private void setSSN(String ssn) {
+		this.ssn = ssn;
+	}
+	
+	private CheckingAccount getCheckingAccount() {
+		return CheckingAccount;
+	}
+	
+	private SavingsAccount getSavingsAccount() {
+		return SavingsAccount;
+	}
+	
+	public String toString() {
+		return "Name: " + firstName + " " + middleName + " " + lastName + 
+				"\nSSN: " + ssn + "\nChecking Account Balance: $" + getCheckingAccount();
+	}
+	
 }
