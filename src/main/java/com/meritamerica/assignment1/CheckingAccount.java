8eaa985d7abc9c33c1e9b27da401df341dc59dcf
Travis Wombles <womblestravis@gmail.com>
2020-10-04 20:52:57 -0500

    Did more work on AccountHolder and CheckingAccount

--- src/main/java/com/meritamerica/assignment1/CheckingAccount.java
+++ src/main/java/com/meritamerica/assignment1/CheckingAccount.java
@@ -2,7 +2,37 @@ package com.meritamerica.assignment1;
 
 public class CheckingAccount {
 	
+	double openingBalance;
+	
 	public CheckingAccount(double openingBalance) {
+		this.openingBalance = openingBalance;
+	}
+	
+	double getBalance() {
+		
+	}
+	
+	double getInterestRate() {
+		
+	}
+	
+	boolean withdraw(double amount) {
+		
+	}
+	
+	boolean deposit(double amount) {
+		
+	}
+	
+	double futureValue(int years) {
+		
+	}
+	
+	public String toString() {
+		
+		return  "Checking Account Balance: $" + getBalance() + "\n" +
+				"Checking Account Interest Rate: " + getInterestRate() + "\n" +
+				"Checking Account Balance in 3 years: $" + futureValue(3);
 		
 	}
 	
