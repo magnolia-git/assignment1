65d9465156478b384b50b71bd914e08125fcb889
Rod <rodwombles@gmail.com>
2020-10-04 23:44:05 -0500

    Changes to be committed: 	modified:   src/main/java/com/meritamerica/assignment1/SavingsAccount.java

--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -8,11 +8,7 @@ public class SavingsAccount {
 	
 	private double accountBalance;
 	private double interestRate;
-	
-	/*
-	 * Constants:
-	 */
-	
+
 	/*
 	 * Constructor:
 	 */
@@ -31,10 +27,20 @@ public class SavingsAccount {
 		return this.accountBalance;
 	}
 	
+	/*
+	 * getInterestRate
+	 * 
+	 * Returns the interest rate.
+	 */
 	public double getInterestRate() {
 		return interestRate;
 	}
 	
+	/*
+	 * withdraw
+	 * 
+	 * Withdraws money from the account. Will return false if the amount withdrew is zero or less.
+	 */
 	public boolean withdraw(double amount) {
 		if(amount <= this.accountBalance) {
 			this.accountBalance -= amount;
@@ -46,6 +52,11 @@ public class SavingsAccount {
 		}
 	}
 	
+	/*
+	 * deposit
+	 * 
+	 * Deposits money into the account. Will return false if the amount deposited is zero or less.
+	 */
 	public boolean deposit(double amount) {
 		if(amount <= 0) {
 			System.out.println("Error, please try again.");
@@ -57,10 +68,22 @@ public class SavingsAccount {
 		}
 	}
 	
+	/*
+	 * futureValue
+	 * 
+	 * This computes how much money an account will have in a vaiable amount of
+	 * years.
+	 */
 	public double futureValue(int years) {
 		return this.accountBalance * (Math.pow(1 + this.interestRate, years));
 	}
 	
+	/*
+	 * truncate
+	 * 
+	 * This will truncate a double to the hundredth's place. Good for displaying
+	 * money.
+	 */
 	private double truncate(double valueToTruncate) {
 		int newValue = (int)valueToTruncate * 100;
 		return valueToTruncate = newValue / 100;
