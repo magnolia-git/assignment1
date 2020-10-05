6e6bdc3027542514fe75b340f1a20ab5852492a4
Rod <rodwombles@gmail.com>
2020-10-04 20:53:57 -0500

    Changes to be committed: 	modified:   src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java 	modified:   src/main/java/com/meritamerica/assignment1/SavingsAccount.java

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,12 @@
 package com.meritamerica.assignment1;
 
 public class MeritAmericaBankApp {
-	
+	public static void main(String[] args) {
+		AccountHolder Bill = new AccountHolder("Masaki","Bill","Sonezaki","123121234",100,1000);
+		System.out.println(Bill);
+		
+		Bill.getCheckingAccount().deposit(500);
+		Bill.getCheckingAccount().withdraw(800);
+		System.out.println(Bill.getCheckingAccount());
+	}
 }
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -2,4 +2,50 @@ package com.meritamerica.assignment1;
 
 public class SavingsAccount {
 	
+	/*
+	 * Instance Variables:
+	 */
+	
+	/*
+	 * Constants:
+	 */
+	
+	/*
+	 * Constructor:
+	 */
+	
+	public SavingsAccount(double OpeningBalance) {
+		
+	}
+	
+	/*
+	 * Methods:
+	 */
+	
+	public double getBalance() {
+		
+	}
+	
+	public double getInterestRate() {
+		
+	}
+	
+	public boolean withdraw(double amount) {
+		
+	}
+	
+	public boolean deposit(double amount) {
+		
+	}
+	
+	public double futureValue(int years) {
+		
+	}
+	
+	public String toString() {
+		return "Savings Account Balance: " + this.AccountBalance + "\n" +
+				"Savings Account Interest Rate: " + this.InterestRate + "\n" +
+				"Savings Account Balance in 3 years: " + futureValue(3);
+	}
+	
 }
